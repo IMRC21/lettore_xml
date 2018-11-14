b0503403a72b7d09d39275deea5d40c55c95672f
cavazzatommaso <cavazzatommaso00@gmail.com>
2018-11-14 20:03:31 +0100

    Working but it's not dynamic on loading file

--- lettura/index.php
+++ lettura/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Un lettore e scrittore di file xml</title>
+    <title>Un lettore di file xml</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
     <!-- <script src="main.js"></script> -->
@@ -13,13 +13,24 @@
     <?php
         if($_POST){
             $percorso = $_POST["file"];
-            echo $percorso;
             $file = file_get_contents("./".$percorso);
-            $oggetto = explode("<persona>",$file);
-            print_r(explode("<cognome>",$oggetto[2]));
+            $data = explode("<persona>",$file);
+            for($i=1;$i<count($data);$i++){
+                preg_match('#<\s*?nome\b[^>]*>(.*?)</nome\b[^>]*>#s', $data[$i], $oggetto[$i]['nome']);
+                preg_match('#<\s*?cognome\b[^>]*>(.*?)</cognome\b[^>]*>#s', $data[$i], $oggetto[$i]['cognome']);
+                preg_match('#<\s*?cf\b[^>]*>(.*?)</cf\b[^>]*>#s', $data[$i], $oggetto[$i]['cf']);
+                preg_match('#<\s*?data\b[^>]*>(.*?)</data\b[^>]*>#s', $data[$i], $oggetto[$i]['data']);
+            }
+
+            for($i=1;$i<count($oggetto)+1;$i++){
+                echo $oggetto[$i]['nome'][1]." ";
+                echo $oggetto[$i]['cognome'][1]." ";
+                echo $oggetto[$i]['cf'][1]." ";
+                echo $oggetto[$i]['data'][1]." ";
+                echo "<br>";
+            }
         }else{
             echo "<form action='index.php' method='POST'>";
-            echo "<label class='testo'>Inserisci il tuo nome</label>";
             echo "<input type='file' name='file'>";
             echo "<input type='submit'>";
             echo "</form>";
