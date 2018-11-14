202c03710c52d719381b1efa7f50f64879fc7a56
imrc <marianoradu@mail.com>
2018-11-14 08:34:39 +0100

    First commit

---
+++ lettore_xml_da/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Un lettore e scrittore di file xml</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- <link rel="stylesheet" type="text/css" media="screen" href="main.css" /> -->
+    <!-- <script src="main.js"></script> -->
+</head>
+<body>
+    <?php
+        if($_post){
+
+        }else{
+            echo "<form action='index.php' method='POST'>";
+            echo "<label class='testo'>Inserisci il tuo nome</label>";
+            echo "<input type='text' name='nome'>";
+            echo "<label class='testo'></label";
+        }
+    ?>
+    
+</body>
+</html>
