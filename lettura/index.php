31a4463afd191f8e2ca3c6edd6e61ec630cc4f2d
cavazzatommaso <cavazzatommaso00@gmail.com>
2018-11-15 16:57:06 +0100

    Color

--- lettura/index.php
+++ lettura/index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Un lettore di file xml</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <link rel="stylesheet" href="main.css">
     <!-- <script src="main.js"></script> -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css">
 </head>
@@ -14,9 +14,11 @@
         if($_POST){
             $percorso = $_POST["file"];
             $file = file_get_contents("./".$percorso);
-            $data = explode("<persona>",$file);
+            
             preg_match_all('~<([^/][^>]*?)>~', $file, $tags);
             $tag = array_unique($tags[1]);
+            $data = explode("<".$tag[1].">",$file);
+            
             for($i=1;$i<count($data);$i++){           
                 for ($c=2; $c<count($tag); $c++) { 
                     preg_match('#<\s*?'.$tag[$c].'\b[^>]*>(.*?)</'.$tag[$c].'\b[^>]*>#s', $data[$i], $oggetto[$i][$tag[$c]]);
@@ -25,11 +27,13 @@
                 }
             }
             for($i=1;$i<count($oggetto)+1;$i++){
+                echo "<span class='head'>".htmlspecialchars("<".$tag[1].">")."</span>"."<br>";
                 for ($c=2; $c<count($tag); $c++) { 
-                    echo htmlspecialchars("<".$tag[$c].">");
+                    echo "<span class='tag'>".htmlspecialchars("<".$tag[$c].">")."</span>";
                     echo $oggetto[$i][$tag[$c]][1];
-                    echo htmlspecialchars("</".$tag[$c].">")."<br>";
+                    echo "<span class='tagEnd'>".htmlspecialchars("</".$tag[$c].">")."</span>"."<br>";
                 }
+                echo "<span class='head'>".htmlspecialchars("</".$tag[1].">")."</span>"."<br>";
                 echo "<br>";
             }
 
