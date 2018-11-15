306ae45b99591c3d5aeee2c3e16764f7e407b42e
cavazzatommaso <cavazzatommaso00@gmail.com>
2018-11-15 13:34:31 +0100

    Dynamic

--- lettura/index.php
+++ lettura/index.php
@@ -15,20 +15,25 @@
             $percorso = $_POST["file"];
             $file = file_get_contents("./".$percorso);
             $data = explode("<persona>",$file);
-            for($i=1;$i<count($data);$i++){
-                preg_match('#<\s*?nome\b[^>]*>(.*?)</nome\b[^>]*>#s', $data[$i], $oggetto[$i]['nome']);
-                preg_match('#<\s*?cognome\b[^>]*>(.*?)</cognome\b[^>]*>#s', $data[$i], $oggetto[$i]['cognome']);
-                preg_match('#<\s*?cf\b[^>]*>(.*?)</cf\b[^>]*>#s', $data[$i], $oggetto[$i]['cf']);
-                preg_match('#<\s*?data\b[^>]*>(.*?)</data\b[^>]*>#s', $data[$i], $oggetto[$i]['data']);
+            preg_match_all('~<([^/][^>]*?)>~', $file, $tags);
+            $tag = array_unique($tags[1]);
+            for($i=1;$i<count($data);$i++){           
+                for ($c=2; $c<count($tag); $c++) { 
+                    preg_match('#<\s*?'.$tag[$c].'\b[^>]*>(.*?)</'.$tag[$c].'\b[^>]*>#s', $data[$i], $oggetto[$i][$tag[$c]]);
+                    if($oggetto[$i][$tag[$c]] == null)
+                    $oggetto[$i][$tag[$c]] = "";
+                }
             }
-
             for($i=1;$i<count($oggetto)+1;$i++){
-                echo $oggetto[$i]['nome'][1]." ";
-                echo $oggetto[$i]['cognome'][1]." ";
-                echo $oggetto[$i]['cf'][1]." ";
-                echo $oggetto[$i]['data'][1]." ";
+                for ($c=2; $c<count($tag); $c++) { 
+                    echo htmlspecialchars("<".$tag[$c].">");
+                    echo $oggetto[$i][$tag[$c]][1];
+                    echo htmlspecialchars("</".$tag[$c].">")."<br>";
+                }
                 echo "<br>";
             }
+
+            
         }else{
             echo "<form action='index.php' method='POST'>";
             echo "<input type='file' name='file'>";
