f61bd276c2a651d490aa3dc19efc1086a3ae582e
cavazzatommaso <cavazzatommaso00@gmail.com>
2018-11-28 21:46:41 +0100

    Added more field in old writer

--- Vecchio/files/index.php
+++ Vecchio/files/index.php
@@ -6,44 +6,61 @@
     <title>Un lettore e scrittore di file xml</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-    <script src="main.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css">
 </head>
 <body>
+    <div class="container">
     <?php
         if($_POST){
-            $nome = $_POST["nome"];
-            $cognome = $_POST["cognome"];
-            echo $cognome;
-            $cf = $_POST["cf"];
             $nomeFile = $_POST["esp"] . ".xml";
-            $stdRiga = "<?xml version='1.0' encoding='UTF-8'?>".PHP_EOL; 
+            $stdRiga = "<?xml version='1.0' encoding='UTF-8'?>".PHP_EOL;
             $root = "<persone>".PHP_EOL;
-            $closeRoot ="</persona>".PHP_EOL;
+            $closeRoot ="</persone>".PHP_EOL;
             $apriTagPersona = "<persona>".PHP_EOL;
             $chiudiTagPersona = "</persona>".PHP_EOL;
-            $nomeStampa = "<nome>" . $nome . "</nome>".PHP_EOL;
-            $cognomeStampa = "<cognome>" . $cognome . "</cognome>".PHP_EOL;
-            $cfStampa = "<cf>" . $cf . "</cf>".PHP_EOL;
-            $stdRiga = $stdRiga . $root . $apriTagPersona . $nomeStampa .$cognomeStampa . $cfStampa . $chiudiTagPersona . $closeRoot;
+            echo $_POST["persone"];
+            if(intval($_POST["persone"])>1){
+                $stdRiga = $root;
+                for($i=1;$i<$_POST["persone"]+1;$i++){
+                    $nome = $_POST["nome".$i];
+                    $cognome = $_POST["cognome".$i];
+                    $cf = $_POST["cf".$i];
+                    $nomeStampa = "<nome>" . $nome . "</nome>".PHP_EOL;
+                    $cognomeStampa = "<cognome>" . $cognome . "</cognome>".PHP_EOL;
+                    $cfStampa = "<cf>" . $cf . "</cf>".PHP_EOL;
+                    $stdRiga = $stdRiga . $apriTagPersona . $nomeStampa .$cognomeStampa . $cfStampa . $chiudiTagPersona;
+                }
+                $stdRiga = $stdRiga . $closeRoot;
+            } else {
+                $nome = $_POST["nome1"];
+                $cognome = $_POST["cognome1"];
+                $cf = $_POST["cf1"];
+                $nomeStampa = "<nome>" . $nome . "</nome>".PHP_EOL;
+                $cognomeStampa = "<cognome>" . $cognome . "</cognome>".PHP_EOL;
+                $cfStampa = "<cf>" . $cf . "</cf>".PHP_EOL;
+                $stdRiga = $stdRiga . $root . $apriTagPersona . $nomeStampa .$cognomeStampa . $cfStampa . $chiudiTagPersona . $closeRoot;
+            }
+            
             $file = file_put_contents($nomeFile,$stdRiga);
         }else{
             
-            echo "<form id='ilForm' action='index.php' method='POST'>";
+            echo "<div id='form'><form id='ilForm' action='index.php' method='POST'>";
             echo "<label class='testo'>Inserisci il tuo nome</label>";
-            echo "<input type='text' name='nome'>";
+            echo "<input type='text' name='nome1'>";
             echo "<label class='testo'>Inserisci il tuo cognome</label>";
-            echo "<input type='text' name='cognome'>";
+            echo "<input type='text' name='cognome1'>";
             echo "<label class='testo'>Inserisci il tuo codice fiscale</label>";
-            echo "<input type='text' name='cf'>";
+            echo "<input type='text' name='cf1'>";
             echo "<label class='testo'>Inserisci il nome di questa esportazione</label>";
             echo "<input type='text' name='esp' value='default'>";
-            echo "<input class='testo' type='button' onclick='aggiungi()' value='Aggiungi un campo'>";
+            echo "<input type='hidden' id='indice' name='persone' value='1'>";
             echo "<input type='submit'>";
-            echo "</form>";
-            echo "<div id='aggiungi-elemento'>CIAO STUPIDO</div>";
+            echo "</form></div>";
+            echo "<input type='button' id='aggiungi' value='Aggiungi' onclick='aggiungi()'>";
         }
     ?>
-    
+    </div>
+    <script src="main.js"></script>
 </body>
 </html>
--- Vecchio/files/main.js
+++ Vecchio/files/main.js
@@ -1,13 +1,20 @@
 function aggiungi(){
-    var contenitore = document.getElementById("nuovo-el");
-    contenitore.append("<h2> Inserisci il tuo nome </h2>");
-    var inp = document.createElement("INPUT");
-    var label_1 = document.createElement("p");
-    var testoLabel_1 =document.createTextNode("Inserisci il nome del nuovo tag");
-    label_1.appendChild(testoLabel_1);
-
-    inp.setAttribute("type","text");
-    inp.setAttribute("class", "nuovo-elemento");
-    contenitore.appendChild(inp);
-
+    console.log("funzione aggiungi");
+    
+    incrementaNumeroEsportazioni = parseInt(document.getElementById("indice").value);
+    incrementaNumeroEsportazioni++;
+    //va rifatto perché usando il parseInt nella variabile abbiamo un numero e non un oggetto
+    //document.getElementById("aggiungi-elementi").value = incrementaNumeroEsportazioni;
+    //ho usato set attribute perché il .value non funzionava
+    document.getElementById("indice").setAttribute("value",incrementaNumeroEsportazioni);
+    //Comincio ad aggiungere gli elementi del form
+    costruisciOutput =
+        "<label class='testo'>Inserisci il tuo nome</label>" + 
+        "<input type='text' name='nome" + incrementaNumeroEsportazioni + "'>" +
+        "<label class='testo'>Inserisci il tuo cognome</label>" +
+        "<input type='text' name='cognome" + incrementaNumeroEsportazioni + "'>" +
+        "<label class='testo'>Inserisci il tuo codice fiscale</label>" +
+        "<input type='text' name='cf"+ incrementaNumeroEsportazioni +"'>";
+    stampa = document.getElementById("ilForm");
+    stampa.innerHTML = stampa.innerHTML + costruisciOutput;
 }
