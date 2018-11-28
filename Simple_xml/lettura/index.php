e7aba1c9d45c978feef067f3dd789b321c2ed370
cavazzatommaso <cavazzatommaso00@gmail.com>
2018-11-28 09:43:26 +0100

    Add lettura

--- Simple_xml/lettura/index.php
+++ Simple_xml/lettura/index.php
@@ -6,16 +6,26 @@
     <title>Un lettore di file xml</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="main.css">
-    <!-- <script src="main.js"></script> -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css">
 </head>
 <body>
     <?php
         if($_POST){
             $percorso = $_POST["file"];
-            $file = file_get_contents("./".$percorso);
-            
-            
+            $xml=simplexml_load_file("./".$percorso);
+            $index = 0;
+            echo "<table>";
+                for($i = 0; $i<count($xml->children()[0]);$i++){
+                    echo "<th>".$xml->children()->children()[$i]->getName()."</th>";
+                }
+            foreach ($xml->children() as $persona) {
+                echo "<tr>";
+                for($i = 0; $i<count($persona);$i++){
+                    echo "<td>".$persona->children()[$i]."</td>";
+                }
+                echo "</tr>";
+             }
+             echo "</table>";
         }else{
             echo "<form action='index.php' method='POST'>";
             echo "<input type='file' name='file'>";
